827eda5d63af805d31a164e3535828b6e0edea0c
sssdark302 <158068173+sssdark302@users.noreply.github.com>
2024-11-17 21:28:50 +0100

    rbitconfig

---
+++ src/main/java/com/example/galton2/config/RabbitConfig.java
@@ -0,0 +1,44 @@
+package com.example.galton2.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    public static final String QUEUE_NAME = "componentesQueue";
+    public static final String EXCHANGE_NAME = "basic_exchange";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("routing.key");
+    }
+
+    // Configurar Jackson2JsonMessageConverter para serializar a JSON
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    // Configurar RabbitTemplate para utilizar el convertidor JSON
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+}
