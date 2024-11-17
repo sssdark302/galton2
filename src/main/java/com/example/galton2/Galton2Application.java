496a4cceea6ebb2e835e426d39b82c337da61ba7
sssdark302 <158068173+sssdark302@users.noreply.github.com>
2024-11-17 21:23:37 +0100

    dependencias

---
+++ src/main/java/com/example/galton2/Galton2Application.java
@@ -0,0 +1,13 @@
+package com.example.galton2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Galton2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Galton2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/galton2/Galton2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.galton2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Galton2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
